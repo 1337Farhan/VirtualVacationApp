6e521e088d17f3699731117f854cd602b44dd34c
1337Farhan <jx@outlook.cl>
2022-12-07 02:35:15 +0300

    adding base skeleton and change prespective

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.Upper}>
+        <Text>Farhan</Text>
+      </View>
+      <View style={styles.Bottom}>
+        <View style={styles.ButtonSize}>
+          <Button
+            style={styles.Button} 
+            title="text" 
+            color={"#FFCA1D"}
+          />
+        </View>
+      </View>
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Upper: {
+    flex:2,
+    backgroundColor: "#4B4AEF",
+    width: "100%",
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Bottom: {
+    flex:1,
+    backgroundColor: "#EBEBEB",
+    width: "100%",
+    alignContent: "center",
+  },
+  ButtonSize: {
+    width: 100,
+    height: 50,
+    margin: 60,
+    alignContent: "center",
+    justifyContent: 'center',
+
+  },
+  Button: {
+
   },
 });
