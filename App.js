04793f03d656ef6b3e4539a939f81e75f75c4507
1337Farhan <jx@outlook.cl>
2022-12-07 04:03:03 +0300

    updated landing page

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     
     <View style={styles.container}>
       <View style={styles.Upper}>
-        <Text>Farhan</Text>
+        <Image source={require('./assets/map.png')} />
       </View>
       <View style={styles.Bottom}>
-        <View style={styles.ButtonSize}>
-          <Button
+        <View style={styles.BottomLeft}>
+          <Text style={styles.LandingText}>You Can Be At Any Place You Want to Be</Text>
+          <Text style={styles.LandingSmall}>Drive, listen to the radio, and explore different cities</Text>
+        </View>
+        <View style={styles.BottomRight}>
+          <TouchableOpacity
             style={styles.Button} 
-            title="text" 
             color={"#FFCA1D"}
-          />
+          >
+            <Text style={styles.buttonText}>Get started</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -29,7 +34,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   Upper: {
-    flex:2,
+    flex:3,
     backgroundColor: "#4B4AEF",
     width: "100%",
     alignContent: "center",
@@ -38,19 +43,51 @@ const styles = StyleSheet.create({
   },
   Bottom: {
     flex:1,
+    flexDirection: "row",
     backgroundColor: "#EBEBEB",
     width: "100%",
     alignContent: "center",
+    alignItems: "stretch",
   },
-  ButtonSize: {
-    width: 100,
-    height: 50,
-    margin: 60,
-    alignContent: "center",
-    justifyContent: 'center',
+  BottomLeft: {
+    height: "50%",
+    width: "50%",
+    // borderColor: "black",
+    // borderRadius: 0,
+    // borderWidth: 1,
+    alignSelf: "flex-start",
+    justifyContent: "center",
+  },
+  BottomRight: {
+    height: "50%",
+    width: "50%",
+    alignSelf: "flex-end",
+    justifyContent: "flex-start",
 
   },
   Button: {
-
+    backgroundColor: "#FFCA1D",
+    borderRadius: 20,
+    padding: 10,
   },
+  buttonText: {
+    fontWeight: "bold",
+    color: "black",
+    textAlign: "center",
+  },
+  LandingText: {
+    fontSize: 25,
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+    marginBottom: 0,
+    marginTop: 25,
+    marginHorizontal: 12,
+    justifyContent: "flex-end"
+  },
+  LandingSmall: {
+    fontSize: 12,
+    fontStyle: "italic",
+    margin: 12,
+    justifyContent: "flex-end"
+  }
 });
